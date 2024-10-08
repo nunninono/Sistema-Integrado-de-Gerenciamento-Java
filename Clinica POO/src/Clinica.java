497308de792b203a41d8a1c148725e0950f840e0
Nunno Wakiyama Diniz Carvalho <142551010+nunninono@users.noreply.github.com>
2024-10-07 23:05:13 -0300

    Update Clinica.java

--- Clinica POO/src/Clinica.java
+++ Clinica POO/src/Clinica.java
@@ -1,13 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Clinica {
     
+    // Atributos da classe
+    private List<Consulta> consultas;
+    private List<Medico> medicos;
+    private List<Paciente> pacientes;
+
+    // Construtores da Clinica
+    public Clinica() { 
+        consultas = new ArrayList<>();
+        medicos = new ArrayList<>();
+        pacientes = new ArrayList<>();
+    }
+
+    // Procedimentos
+
+    // Adicionar consulta na lista
+    public void adicionarConsulta(Consulta consulta) {
+        consultas.add(consulta);
+    }
+
+    // Adicionar medico na lista
+    public void adicionarMedico(Medico medico) {
+        medicos.add(medico);
+    }
+
+    // Adicionar paciente na lista
+    public void adicionarPaciente(Paciente paciente) {
+        pacientes.add(paciente);
+    }
+
+    // Exibe os dados da consulta
+    public void exibirDadosConsulta(Consulta consulta) {
+        System.out.println("Dados da Consulta:");
+        System.out.println("Data: " + consulta.getData());
+        System.out.println("Hora: " + consulta.getHora());
+        System.out.println("Realizada: " + consulta.isRealizada());
+    }
+
+    public void exibirDadosMedico(Medico medico) {
+        System.out.println("Dados do Médico:");
+        System.out.println("Nome: " + medico.getNomeMedico());
+        System.out.println("CRM: " + medico.getCrm());
+        System.out.println("Especialização: " + medico.getEspecializacao());
+        System.out.println("Disponível: " + medico.getDisponibilidade());
+    }
+
     public static void main (String[] args) {
-        Consulta consulta = new Consulta("27/08/2024", "16:00", "dor de garganta", false);
+
+        // Cria uma isntância da Clinica
+        Clinica clinica = new Clinica();
+
+        // Cria um Paciente, Medico e Consulta
         Paciente paciente = new Paciente("Nunno", 19, "Tosse", true, "+55(81)99607-6104", "B-");
         Medico medico = new Medico("João", true, "Cardiologista", 123456);
+        // Inicializar Consulta apenas apos inicializar os objetos Paciente e Medico, pq ambos compõe a Consulta
+        Consulta consulta = new Consulta("27/08/2024", "16:00", false, medico, paciente);
+
+        // Adiciona o Paciente, Medico e Consulta à Clinica
+        clinica.adicionarPaciente(paciente);
+        clinica.adicionarMedico(medico);
+        clinica.adicionarConsulta(consulta);
 
         System.out.println(consulta.getData());
         System.out.println(paciente.getNomePaciente());
-        System.out.println(medico.getNomeMedico());
+        System.out.println(medico.getDisponibilidade());
     }
-    
 }
